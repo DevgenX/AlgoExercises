f7ea664a3eef58bd93a278c7d4958f9b9a800318
devgenx <sebastian110420@gmail.com>
2023-01-04 14:38:58 -0500

    Update Addup.js

--- Addup.js
+++ Addup.js
@@ -1,22 +1,17 @@
 const addUp = (n) => {
 	return n * (n + 1) / 2
 }
-
-
 const t1 = performance.now()
 addUp(1000000)
 const t2 = performance.now()
-
 console.log(`Time elapsed ${(t2 - t1) } ms`)
 
 const addUpTwo = (n) => {
 
 	let total = 0;
-
-	for(let i = 0; i < n; i++) {
+	for(let i = 1; i < n; i++) {
 		total+=i
 	}
-
 	return total;
 }
 
