867aef9f113e5c5d744fc8a2d74f0e5a32dc858d
devgenx <sebastian110420@gmail.com>
2023-01-18 14:52:37 -0500

    BruteForce TwoSum

---
+++ twoSum(BruteForce).js
@@ -0,0 +1,19 @@
+const twoSum = (nums, target) => {
+    
+    let result = []
+
+    for(let i = 0; i < nums.length; i++) {
+        for(let j = i + 1; j < nums.length; j++) {
+            if(nums[i] + nums[j] === target)
+            result.push(i, j)
+        }
+    }
+    return result
+
+};
+
+const nums = [2,7,11,15]
+const target = 9
+
+console.log(twoSum(nums, target))
+
