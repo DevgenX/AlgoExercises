aeda8810a0f6fe8fbe98f0c7d89ded7df7c118be
devgenx <sebastian110420@gmail.com>
2023-01-18 16:00:22 -0500

    Added Time complexity

--- plusOneBruteforce.js
+++ plusOneBruteforce.js
@@ -1,3 +1,5 @@
+
+// slower but works with bigger data
 const plusOne = (digits) => {
 
 	for(let i = digits.length -1; i >= 0; i--) {
@@ -13,6 +15,12 @@ const plusOne = (digits) => {
 	return digits
 };
 
+const digit = [4,3,2,1] 
+const t1 = performance.now()
+console.log(plusOne(digit)) // should log : [4,3,2,2]
+const t2 = performance.now()
+
+console.log(`Time elapsed ${t2-t1} ms`) // 2.1 ms
 
 // Good solution for small data, fails to convert into num when the array has length more than 10
 const plusOne2 = (digits) => {
@@ -22,13 +30,23 @@ const plusOne2 = (digits) => {
 	return (parseInt(digits.join('')) + 1).toString().split('').map(Number)
 }
 
-const digit = [4,3,2,1] 
-const digit2 = [1,2,3] 
-const digit3 = [9] 
-const digit4 = [6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]
+const digits = [4,3,2,1] 
+const t3 = performance.now()
+console.log(plusOne2(digits))
+const t4 = performance.now()
 
-console.log(plusOne(digit)) // should log : [4,3,2,2]
-console.log(plusOne(digit2)) // should log : [1,2,4]
-console.log(plusOne(digit3)) // should log : [ 1, 0]
-console.log(plusOne(digit4)) // [6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,4]
+console.log(`Time elapsed ${t4-t3} ms`) // 0.06ms
+
+
+
+
+// const digit = [4,3,2,1] 
+// const digit2 = [1,2,3] 
+// const digit3 = [9] 
+// const digit4 = [6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]
+
+// console.log(plusOne(digit)) // should log : [4,3,2,2]
+// console.log(plusOne(digit2)) // should log : [1,2,4]
+// console.log(plusOne(digit3)) // should log : [ 1, 0]
+// console.log(plusOne(digit4)) // [6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,4]
 
