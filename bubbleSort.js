16ed78877b1af0197a78792da5051fbf4ec0a6e6
devgenx <sebastian110420@gmail.com>
2023-01-31 14:32:54 -0500

    Create bubbleSort.js

---
+++ bubbleSort.js
@@ -0,0 +1,22 @@
+const bubbleSort = (arr) => {
+	
+	
+	for(let i = arr.length; i > 0; i--) {
+
+		let noSwaps;
+		for(let j = 0; j < i - 1; j++) {
+			noSwaps = true;
+			if(arr[j] < arr[j+1]) {
+				// console.log(arr, arr[j], arr[j+1])
+				let temp = arr[j]
+				arr[j] = arr[j+1]
+				arr[j+1] = temp
+				noSwaps= false;
+			}
+		}
+		if(noSwaps) break;
+	}
+	return arr
+}
+
+console.log(bubbleSort([35,37,45,29,8, 9,27,36,13,4,3,19]))
